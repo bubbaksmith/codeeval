02d8f67fbd1b8303c6b25326d9b2e1c541fe4a32
Brian Smith <brian.smith.keith@gmail.com>
2016-06-06 19:58:35 -0400

    First Non-Repeated Character

---
+++ ruby/moderate/first_non-repeated_character.rb
@@ -0,0 +1,18 @@
+# https://www.codeeval.com/open_challenges/12/
+
+# Write a program which finds the first non-repeated character in a string.
+
+# File.open(ARGV[0]).each_line do |line|
+require 'colorize'
+require 'debugger2'
+require 'byebug'
+File.open('/Users/brian/codeeval/input.txt').each_line do |line|
+	next if line.empty?
+
+	line.each_char do |char|
+
+		(print char + "\n"; break) unless line[/#{char}\w*#{char}/]
+		
+	end
+
+end
