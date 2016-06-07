b0a63b243275a2798c27310fbc8acdda20eb3577
Brian Smith <brian.smith.keith@gmail.com>
2016-06-06 20:01:10 -0400

    one liners are cooler to look at but harder to follow

--- ruby/moderate/first_non-repeated_character.rb
+++ ruby/moderate/first_non-repeated_character.rb
@@ -3,16 +3,10 @@
 # Write a program which finds the first non-repeated character in a string.
 
 # File.open(ARGV[0]).each_line do |line|
-require 'colorize'
-require 'debugger2'
-require 'byebug'
+# require 'colorize'
+# require 'debugger2'
+# require 'byebug'
 File.open('/Users/brian/codeeval/input.txt').each_line do |line|
 	next if line.empty?
-
-	line.each_char do |char|
-
-		(print char + "\n"; break) unless line[/#{char}\w*#{char}/]
-		
-	end
-
+	line.each_char {|char| (print char + "\n"; break) unless line[/#{char}\w*#{char}/] }
 end
